febbbe65f437e048571766319763d00aacc86b8b
Cameron Erdman <cmerdman@outlook.com>
2020-10-30 17:54:24 -0400

    oh?

--- WebScraping/Selenium_NextDoor_Exercise.R
+++ WebScraping/Selenium_NextDoor_Exercise.R
@@ -2,7 +2,7 @@ library(RSelenium)
 library(rvest)
 library(dplyr)
 
-rD = rsDriver(browser="firefox", port=4546L, verbose=F)                
+rD = rsDriver(browser="firefox", port=4544L, verbose=F)                
 remDr = rD[["client"]]
 remDr$navigate("https://nextdoor.com/")
 
@@ -10,9 +10,8 @@ email = "bdaa.nextdoor+brute@gmail.com"
 address = "156 E 13th"
 
 remDr$findElement(using = "css", value = "input[class = 'select-display-block']")$sendKeysToElement(list(address))
-Sys.sleep(1)
-remDr$sendKeysToElement(list("R", key = "enter"))
-remDr$sendKeysToElement(list("R", key = "enter"))
+Sys.sleep(2)
+remDr$findElement(using = "css", value = "input[class = 'select-display-block']")$sendKeysToElement(list(key = ""))
 remDr$findElement(using = "css", value = "input[class = 'nux-signup-address-chooser-email-field fs-block']")$sendKeysToElement(list(email))
 remDr$findElement(using = "css", value = "button[class = 'css-3tna2']")$clickElement()
 Sys.sleep(5)
