f1bf0f6d5b133df2248c0d2857d0ea20040d7278
Cameron Erdman <cmerdman@outlook.com>
2021-03-02 18:13:22 -0500

    init

---
+++ python/chess.py
@@ -0,0 +1,13 @@
+"""
+A program to build an interactive chess bot to challenge the user to a game of chess
+Needs:
+    > A board
+    > Pieces and coordinates
+    > Movements
+    > Captures and Wins
+    > Rules and enforecement
+    > AI to play against
+"""
+
+
+
