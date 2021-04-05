c571419d01f9725eff910186f3367ac04ab256e1
Cmerdman <cmerdman@outlook.com>
2021-04-05 12:07:33 -0400

    cs

--- AEP/script.R
+++ AEP/script.R
@@ -1,12 +1,10 @@
 library("tidyverse")
 library("readxl")
-invoices = read_xlsx("AEP/data/Invoices.xlsx")
-summary = read_xlsx("AEP/data/Summary.xlsx")
-income = read.csv("data/annualIncomeSummary.csv")
-Zip <- income[, "Zip"]
-incomeNew <- cbind(Zip, income[, 52:55])
-income <- distinct(incomeNew)
-
+invoices = read_xlsx("data/Invoices.xlsx")
+summary = read_xlsx("data/Summary.xlsx")
+income = read.csv("data/income_data.csv")
+income <- income[,-c(1)]
+annIncome <- read.csv("data/annualIncomeSummary.csv")
 
 ##### Summary Statistics #####
 
@@ -26,7 +24,7 @@ mean(invoices[["Price_MWh"]])
 median(invoices[["Price_MWh"]])
 min(invoices[["Price_MWh"]])
 max(invoices[["Price_MWh"]])
-# mean margin = 56.6396, median = 55.9, min = 0, max = 77.31
+# mean Price_MWh = 56.6396, median = 55.9, min = 0, max = 77.31
 
 length(unique(invoices[["CDSID"]]))
 # 2375 unique ID's
@@ -45,7 +43,48 @@ min(e)
 max(e)
 # mean of invoice count = 31.72337, median = 34, min = 16, max = 52
 
-
+mean(income[["medIncome"]])
+median(income[["medIncome"]])
+min(income[["medIncome"]])
+max(income[["medIncome"]])
+#mean median income = 59314.57, median = 56414, min = 20221, max = 142689
 
 #### exploratory Analysis ####
+
+
+##Exploring relation of margin and zip code, margin and zip code income##
+
+#margin and median income
+medInc <- annIncome[, "medIncome"]
+margin <- summary[, "Margin_per_invoice"]
+margin <- unlist(margin)
+relation <- lm(medInc ~ margin)
+plot(margin , medInc , col = "blue", main = "Median income and Margin regression")
+abline(lm(medInc ~ margin), cex = 1.3, pch = 16, xlab = "Median Income", ylab = "margin of profitability")
+
+#margin and zip code
+zipcode <- summary[, "Zip"]
+margin <- summary[, "Margin_per_invoice"]
+margin <- unlist(margin)
+plot(zipcode, margin, main = "Scatter plot by zipcode and margin", xlab = "Zip Code", ylab = "Margin of Profitability", pch = 19)
+zip_uni = unique(zipcode)
+zips <-c()
+means <- c()
+for(x in 1:407){
+  zips <-c()
+  for(i in 1:2375){
+    temp = zipcode[i:i,1:1]
+    if(zip_uni[x:x, 1:1] == temp){
+      zips <- c(zips, margin[i:i])
+    }
+  }
+  means<- c(means, setNames(mean(as.numeric(zips)), temp))
+  #each label is the same zip, idk why
+}
+
+
+
+
+
+
 
